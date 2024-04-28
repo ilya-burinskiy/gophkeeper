b0aec8b9a49c5a593e84bef1a96fa56f26023fbb
Ilya Burinsky <burinskiy992@gmail.com>
2024-04-28 21:35:27 +0300

    GophkeeperClient.AuthenticateUser()

--- client/api/client.go
+++ client/api/client.go
@@ -11,6 +11,7 @@ import (
 
 type GophkeeperClient struct {
 	baseURL    string
+	jwt        string
 	httpClient http.Client
 }
 
@@ -53,4 +54,44 @@ func (client *GophkeeperClient) RegisterUser(ctx context.Context, login, passwor
 	}
 
 	return nil
+}
+
+func (client *GophkeeperClient) AuthenticateUser(ctx context.Context, login, password string) (string, error) {
+	reqBody, err := json.Marshal(
+		UserCredentials{
+			Login:    login,
+			Password: password,
+		},
+	)
+	if err != nil {
+		return "", fmt.Errorf("failed encode request body: %w", err)
+	}
+	req, err := http.NewRequest(
+		http.MethodPost,
+		client.baseURL+"/api/user/login",
+		bytes.NewReader(reqBody),
+	)
+	if err != nil {
+		return "", fmt.Errorf("failed to create request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	req.Header.Set("Accept", "application/json; charset=utf-8")
+
+	resp, err := client.httpClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to do request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.New("failed to authenticate user")
+	}
+
+	for _, cookie := range resp.Cookies() {
+		if cookie.Name == "jwt" {
+			return cookie.Value, nil
+		}
+	}
+
+	return "", errors.New("failed to get JWT from response")
 }
